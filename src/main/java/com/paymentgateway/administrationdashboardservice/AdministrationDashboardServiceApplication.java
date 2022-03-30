00ceae6441415c4bdbb0215ccfbb214a938c7bdc
Md. Shakil Hossain <shakilhossainpavel@gmail.com>
2022-03-31 02:06:56 +0600

    basic structure

---
+++ src/main/java/com/paymentgateway/administrationdashboardservice/AdministrationDashboardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paymentgateway.administrationdashboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministrationDashboardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdministrationDashboardServiceApplication.class, args);
+    }
+
+}
